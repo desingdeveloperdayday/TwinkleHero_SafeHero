b429d427de101f6a62d099fc15303cb31dd2205a
onemask <onemask@naver.com>
2019-05-26 18:49:30 +0900

    material design library 추가. app_bar layout 추가. string.xml 추가.

---
+++ android/app/src/main/java/com/ddd/twinkle/safehero/signup/ui/SignupActivity.kt
@@ -0,0 +1,13 @@
+package com.ddd.twinkle.safehero.signup.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.ddd.twinkle.safehero.R
+
+class SignupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
