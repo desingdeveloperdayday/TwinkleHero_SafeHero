9936ea45f48b1e170a4d7e741783e924fa678b6e
onemask <onemask@naver.com>
2019-06-01 19:48:06 +0900

    본인인증 View 수정 AppbarLayout 수정.

--- android/app/src/main/java/com/ddd/twinkle/safehero/SplashActivity.kt
+++ android/app/src/main/java/com/ddd/twinkle/safehero/SplashActivity.kt
@@ -1,8 +1,8 @@
 package com.ddd.twinkle.safehero
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ddd.twinkle.safehero.signup.ui.newIntentSignupActivity
 
 class SplashActivity : AppCompatActivity() {
 
@@ -14,6 +14,7 @@ class SplashActivity : AppCompatActivity() {
     private fun goToMainActivity() {
 
         //todo("회원가입 필요한 회원인지 아닌지 분기 처리 필요함")
+        startActivity(newIntentSignupActivity())
 
 
         /*startActivity(newIntentMainActivity())
--- android/app/src/main/java/com/ddd/twinkle/safehero/signup/ui/SignupActivity.kt
+++ android/app/src/main/java/com/ddd/twinkle/safehero/signup/ui/SignupActivity.kt
@@ -1,13 +1,28 @@
 package com.ddd.twinkle.safehero.signup.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.view.Gravity
+import androidx.appcompat.app.AppCompatActivity
 import com.ddd.twinkle.safehero.R
+import kotlinx.android.synthetic.main.app_bar.*
 
+fun Context.newIntentSignupActivity() : Intent {
+    val intent = Intent(this,SignupActivity::class.java)
+    return intent
+}
 class SignupActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+        setuptoolbar()
     }
+
+    private fun setuptoolbar() {
+        toolbar_text.text="본인 인증"
+    }
+
+
 }
