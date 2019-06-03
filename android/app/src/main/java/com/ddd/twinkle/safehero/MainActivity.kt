1034f881fe3fdacb9581034b4df25e3cd90a557c
onemask <onemask@naver.com>
2019-06-03 22:58:55 +0900

    BottomNavigation View 추가. 긴급 화면 View 추가.

--- android/app/src/main/java/com/ddd/twinkle/safehero/MainActivity.kt
+++ android/app/src/main/java/com/ddd/twinkle/safehero/MainActivity.kt
@@ -2,18 +2,51 @@ package com.ddd.twinkle.safehero
 
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_bottom_navigation.*
 
 fun Context.newIntentMainActivity() : Intent{
     val intent =Intent(this,MainActivity::class.java)
     return intent
 }
 
+private var mOnNavigationItemSelectedListener  = null
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupBottomNavigationView()
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+
+
+    }
+
+    private fun setupBottomNavigationView() {
+         val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.emergency -> {
+                    message.setText(R.string.title_home)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.safe_sservice -> {
+                    message.setText(R.string.title_dashboard)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.community -> {
+                    message.setText(R.string.title_notifications)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.and_more -> {
+                    message.setText(R.string.title_notifications)
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+
+        }
+
     }
 }
