24a2d99f438065a694b9b1d075e521818c58e3cf
onemask <onemask@naver.com>
2019-06-07 15:34:44 +0900

    Tiber Library 추가

--- android/app/src/main/java/com/ddd/twinkle/safehero/SplashActivity.kt
+++ android/app/src/main/java/com/ddd/twinkle/safehero/SplashActivity.kt
@@ -3,12 +3,14 @@ package com.ddd.twinkle.safehero
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.ddd.twinkle.safehero.signup.ui.newIntentSignupActivity
+import timber.log.Timber
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
             goToMainActivity()
+            setupTimber()
     }
 
     private fun goToMainActivity() {
@@ -16,8 +18,11 @@ class SplashActivity : AppCompatActivity() {
         //todo("회원가입 필요한 회원인지 아닌지 분기 처리 필요함")
         startActivity(newIntentSignupActivity())
 
-
         /*startActivity(newIntentMainActivity())
         finish()*/
+    }
+
+    private fun setupTimber(){
+        Timber.plant(Timber.DebugTree())
     }
 }
