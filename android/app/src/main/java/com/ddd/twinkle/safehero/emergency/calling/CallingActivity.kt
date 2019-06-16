feca48d0758cef12f01a76907d3ffed3d26d62dd
onemask <onemask@naver.com>
2019-06-16 21:44:34 +0900

    긴급 화면 > 나에게 전화 걸기 > 진입점 추가

--- android/app/src/main/java/com/ddd/twinkle/safehero/emergency/calling/CallingActivity.kt
+++ android/app/src/main/java/com/ddd/twinkle/safehero/emergency/calling/CallingActivity.kt
@@ -1,8 +1,15 @@
 package com.ddd.twinkle.safehero.emergency.calling
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.ddd.twinkle.safehero.R
+
+fun Context.newIntentCallingActivity() : Intent{
+    val intent = Intent(this,CallingActivity::class.java)
+    return intent
+}
 
 class CallingActivity : AppCompatActivity() {
 
