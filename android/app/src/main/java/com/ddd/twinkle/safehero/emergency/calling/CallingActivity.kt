faa9e89d1ae90513ea687d95ea647c06a35d1ac5
onemask <onemask@naver.com>
2019-06-19 00:59:58 +0900

    나에게 전화 걸기 > 전화버튼 이미지 > 녹음 기능 추가

--- android/app/src/main/java/com/ddd/twinkle/safehero/emergency/calling/CallingActivity.kt
+++ android/app/src/main/java/com/ddd/twinkle/safehero/emergency/calling/CallingActivity.kt
@@ -2,9 +2,12 @@ package com.ddd.twinkle.safehero.emergency.calling
 
 import android.content.Context
 import android.content.Intent
+import android.media.MediaRecorder
 import android.os.Bundle
+import android.os.Environment
 import androidx.appcompat.app.AppCompatActivity
 import com.ddd.twinkle.safehero.R
+import timber.log.Timber
 
 fun Context.newIntentCallingActivity() : Intent{
     val intent = Intent(this,CallingActivity::class.java)
@@ -13,8 +16,39 @@ fun Context.newIntentCallingActivity() : Intent{
 
 class CallingActivity : AppCompatActivity() {
 
+    lateinit var audioRecorder: MediaRecorder
+    lateinit var mPath : String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calling)
+
+        setupButton()
+        setupRecorder()
+        setupOutputFile()
+    }
+
+    private fun setupButton() {
+
+    }
+
+    private fun setupRecorder(){
+        audioRecorder= MediaRecorder().apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.AAC_ADTS)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+            setOutputFile(setupOutputFile())
+        }
+        try {
+            audioRecorder.prepare()
+        }catch (e : Exception){
+            Timber.e("${e.printStackTrace()}")
+        }
+    }
+
+    private fun setupOutputFile() : String{
+        mPath = Environment.getExternalStorageDirectory().absolutePath +"/record.aac"
+        Timber.d("mPath is $mPath")
+        return mPath
     }
 }
