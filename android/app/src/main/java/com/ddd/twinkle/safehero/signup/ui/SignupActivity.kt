f723404d4439fbfad9086cbe090738c1f5408baa
onemask <onemask@naver.com>
2019-06-21 21:46:44 +0900

    SignupActivity DownCountDown 추가, SignupLayout> 카운트 다운 레이아웃 수정 필요

--- android/app/src/main/java/com/ddd/twinkle/safehero/signup/ui/SignupActivity.kt
+++ android/app/src/main/java/com/ddd/twinkle/safehero/signup/ui/SignupActivity.kt
@@ -3,10 +3,14 @@ package com.ddd.twinkle.safehero.signup.ui
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.appcompat.app.AppCompatActivity
 import com.ddd.twinkle.safehero.R
+import com.ddd.twinkle.safehero.utills.toReadableTime
 import kotlinx.android.synthetic.main.activity_signup.*
 import kotlinx.android.synthetic.main.app_bar.*
+import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 fun Context.newIntentSignupActivity() : Intent {
     val intent = Intent(this,SignupActivity::class.java)
@@ -17,12 +21,14 @@ class SignupActivity : AppCompatActivity() {
     private var naem : String? = ""
     private var phone_num : String? = ""
     private var confirm_num: String? =""
+    private var timer : CountDownTimer? =null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
         setuptoolbar()
         setupButton()
+        Timber.d("22")
     }
 
     private fun setuptoolbar() {
@@ -31,15 +37,28 @@ class SignupActivity : AppCompatActivity() {
 
     private fun setupButton() {
         send_auth_num.setOnClickListener {
-            //todo("인증 번호 발소오디는 api 필요.")
+            //todo("인증 번호 발송하는 는 api 필요.")
+            startTimer()
+
         }
         button_nextStep.setOnClickListener {
             startActivity(newIntentAddParentActivityu())
         }
 
+
+
     }
 
-
+    private fun startTimer() {
+       timer = object : CountDownTimer(180000,1000){
+            override fun onFinish() {
+                text_timer.text="0:00"
+            }
+            override fun onTick(millisUntilFinished: Long) {
+                text_timer.text = toReadableTime(millisUntilFinished)
+            }
+       }.start()
+    }
 
 
 }
