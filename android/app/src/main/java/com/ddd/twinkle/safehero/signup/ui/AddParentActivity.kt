2a9a0a7d4bab23e4850283c8a4bbfa1eea3851ff
onemask <onemask@naver.com>
2019-06-25 00:27:29 +0900

    보호자 설정 UI 추가, 변경

--- android/app/src/main/java/com/ddd/twinkle/safehero/signup/ui/AddParentActivity.kt
+++ android/app/src/main/java/com/ddd/twinkle/safehero/signup/ui/AddParentActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.ddd.twinkle.safehero.R
+import kotlinx.android.synthetic.main.app_bar.*
 
 fun Context.newIntentAddParentActivityu() : Intent{
     val intent = Intent(this,AddParentActivity::class.java)
@@ -17,6 +18,11 @@ class AddParentActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_parent)
 
+        setupToolbar()
+    }
+
+    private fun setupToolbar() {
+        toolbar_text.text="보호자 설정"
     }
 
 }
