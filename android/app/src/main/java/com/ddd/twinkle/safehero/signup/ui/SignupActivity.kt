4bdbabce1aa01102c85d23863c78a91716d49bd2
onemask <onemask@naver.com>
2019-06-24 23:56:57 +0900

    회원 가입 UI 변경, 버튼 유효성 로직 추가해야함

--- android/app/src/main/java/com/ddd/twinkle/safehero/signup/ui/SignupActivity.kt
+++ android/app/src/main/java/com/ddd/twinkle/safehero/signup/ui/SignupActivity.kt
@@ -5,12 +5,12 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.CountDownTimer
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.ddd.twinkle.safehero.R
 import com.ddd.twinkle.safehero.utills.toReadableTime
+import com.jakewharton.rxbinding3.widget.textChangeEvents
 import kotlinx.android.synthetic.main.activity_signup.*
 import kotlinx.android.synthetic.main.app_bar.*
-import timber.log.Timber
-import java.util.concurrent.TimeUnit
 
 fun Context.newIntentSignupActivity() : Intent {
     val intent = Intent(this,SignupActivity::class.java)
@@ -28,8 +28,18 @@ class SignupActivity : AppCompatActivity() {
         setContentView(R.layout.activity_signup)
         setuptoolbar()
         setupButton()
-        Timber.d("22")
+        checkButtonValid()
     }
+
+    private fun checkButtonValid() {
+      val nameCheck =  edit_name.textChangeEvents()
+           .map { it.text.isNotEmpty() }
+      val phoneCheck = edit_phone.textChangeEvents()
+          .map { it.text.isNotEmpty() }
+      val authNumCheck = edit_auth_num.textChangeEvents()
+          .map { it.text.isNotEmpty() }
+    }
+
 
     private fun setuptoolbar() {
         toolbar_text.text="본인 인증"
@@ -39,14 +49,16 @@ class SignupActivity : AppCompatActivity() {
         send_auth_num.setOnClickListener {
             //todo("인증 번호 발송하는 는 api 필요.")
             startTimer()
-
         }
+
+        button_nextStep.setBackgroundColor(ContextCompat.getColor(applicationContext,R.color.alert_color))
+            .run {
+                button_nextStep.isEnabled =true
+            }
+
         button_nextStep.setOnClickListener {
             startActivity(newIntentAddParentActivityu())
         }
-
-
-
     }
 
     private fun startTimer() {
