59f1e1528fee785e54be9566017f7b68cd958709
onemask <onemask@naver.com>
2019-07-02 22:36:18 +0900

    [Android]Search Activity_layout 수정

--- android/app/src/main/java/com/ddd/twinkle/safehero/monitoring/SearchActivity.kt
+++ android/app/src/main/java/com/ddd/twinkle/safehero/monitoring/SearchActivity.kt
@@ -1,73 +1,62 @@
 package com.ddd.twinkle.safehero.monitoring
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
 import com.ddd.twinkle.safehero.R
-import kotlinx.android.synthetic.main.app_bar.*
+import com.google.android.material.tabs.TabLayout
 
 
-class SearchActivity : AppCompatActivity() {
+
+
+
+class SearchActivity : AppCompatActivity(),SearchView.OnQueryTextListener {
+
+    lateinit var searchManager: SearchManager
+    lateinit var searchMenuItem: MenuItem
+    lateinit var searchView: SearchView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
-        setupToolbar()
+
+        addTabItem()
+
+
+
     }
 
-    private fun setupToolbar() {
-        toolbar.inflateMenu(R.menu.options_menu)
+    private fun addTabItem() {
+        val tabLayout = findViewById<TabLayout>(R.id.layout_tab)
+        tabLayout.addTab(tabLayout.newTab().setText("TAB-3"))
+        tabLayout.addTab(tabLayout.newTab().setText("TAB-4"))
 
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.options_menu, menu)
+        val inflater: MenuInflater = menuInflater.apply {
+            this.inflate(com.ddd.twinkle.safehero.R.menu.search_menu, menu)
+        }
+
+        searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        searchMenuItem = menu.findItem(com.ddd.twinkle.safehero.R.id.search)
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.isSubmitButtonEnabled = true
+        searchView.setOnQueryTextListener(this)
         return true
-
-        /*mSearch = menu.findItem(R.id.search)
-        //menu Item Click
-        mSearch.setOnActionExpandListener(object : MenuItem.OnActionExpandListener {
-            override fun onMenuItemActionExpand(item: MenuItem?): Boolean {
-                txtstatus.setText("현재 상태 : 확장됨")
-                return true
-            }
-
-            override fun onMenuItemActionCollapse(item: MenuItem?): Boolean {
-                txtstatus.setText("현재 상태 :축소 됨")
-                return true
-            }
-        })
-
-        //menuItem을 이용해서 SearchView 변수 생성
-        val sv = mSearch.actionView as SearchView
-        //확인버튼 활성화
-        sv.setSubmitButtonEnabled(true)
-
-        //SearchView의 검색 이벤트
-        sv.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
-            //검색 버튼 눌렸을 경우
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                txtresult.text = query + "를 검색합니다."
-                return true
-            }
-
-            //텍스트가 바뀔때마다 호출
-            override fun onQueryTextChange(newText: String?): Boolean {
-                txtsearch.text = "검색식 : $newText"
-                return true
-            }
-        })*/
     }
 
+    override fun onQueryTextSubmit(query: String?): Boolean {
+        return false
+    }
 
-
-   /* // 검색 확장,축소를 버튼으로 생성
-    fun mOnClick(v: View) {
-        when (v.id) {
-            R.id.btnexpand  -> mSearch.expandActionView()
-            R.id.btncollapse -> mSearch.collapseActionView()
-        }
-    }*/
-
+    override fun onQueryTextChange(newText: String?): Boolean {
+        return true
+    }
 }
-
